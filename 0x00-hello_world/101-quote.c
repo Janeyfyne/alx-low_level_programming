bc7b9231a93571dbcbda4dff18cc6faa6e8f534d
Janeyfyne <nzekwechika2@gmail.com>
2023-02-16 12:03:26 -0800

    quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,20 +1,13 @@
-#include<stdio.h>
-#include<unistd.h>
-
+#include <stdio.h>
+#include <unistd.h>
 /**
- * main - run all functions
- *
- * Description: Change standard error message
- * Return: Return 1 and exit
- */
+* main - main function in the task
+*
+*
+*Return: return 1
+*/
 int main(void)
 {
-	int l;
-
-	l =
-	  sizeof("and that piece of art is useful - Dora Korpar, 2015-10-19");
-
-	write(2,
-	      "and that piece of art is useful - Dora Korpar, 2015-10-19\n", l);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
